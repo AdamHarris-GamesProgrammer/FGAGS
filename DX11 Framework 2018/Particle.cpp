9ae698b7698cf2af47a9aebf7ac9272e3095c370
Adam Harris <adam_harris@hotmail.com>
2021-02-01 09:59:51 +0000

    0x127: Implemented basic particle physics model

---
+++ DX11 Framework 2018/Particle.cpp
@@ -0,0 +1,44 @@
+#include "Particle.h"
+
+void Particle::Integrate(real dt)
+{
+	if (InverseMass() <= 0.0) return;
+
+	assert(dt > 0.0);
+
+	_position.AddScaledVector(_velocity, dt);
+
+	Vector3 resultingAcceleration = _acceleration;
+
+	Velocity().AddScaledVector(resultingAcceleration, dt);
+
+	Velocity(_velocity * real_pow(_damping, dt));
+
+	ClearAccumulator();
+}
+
+void Particle::ClearAccumulator()
+{
+	_forceAccumulator = Vector3();
+}
+
+void Particle::AddForce(const Vector3& force)
+{
+	_forceAccumulator += force;
+}
+
+real Particle::Mass() const
+{
+	if (_inverseMass == 0) {
+		return REAL_MAX;
+	}
+	else {
+		return ((real)1.0) / _inverseMass;
+	}
+}
+
+void Particle::Mass(const real mass)
+{
+	assert(mass != 0);
+	_inverseMass = ((real)1.0) / mass;
+}
--- DX11 Framework 2018/Particle.h
+++ DX11 Framework 2018/Particle.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "Vector3.h"
+#include "Precision.h"
+#include <assert.h>
+
 
 class Particle
 {
@@ -14,5 +17,39 @@ protected:
 	//Holds the acceleration of the particle.
 	Vector3 _acceleration;
 
+	Vector3 _forceAccumulator;
+
+	//Holds the amount of damping that is applied to linear motion. Acts as a drag force
+	real _damping;
+
+	//Holds the inverse mass of the particle it is more useful to hold the inverse mass because integration is simpler
+	real _inverseMass;
+
+
+
+public:
+	void Integrate(real dt);
+
+	void ClearAccumulator();
+
+	void AddForce(const Vector3& force);
+
+
+
+	real Mass() const;
+	void Mass(const real mass);
+	bool HasFiniteMass() const {
+		return _inverseMass >= 0.0f;
+	}
+	real InverseMass() const { return _inverseMass; }
+	void InverseMass(real val) { _inverseMass = val; }
+	real Damping() const { return _damping; }
+	void Damping(real val) { _damping = val; }
+	Vector3 Acceleration() const { return _acceleration; }
+	void Acceleration(Vector3 val) { _acceleration = val; }
+	Vector3 Velocity() const { return _velocity; }
+	void Velocity(Vector3 val) { _velocity = val; }
+	Vector3 Position() const { return _position; }
+	void Position(Vector3 val) { _position = val; }
 };
 
