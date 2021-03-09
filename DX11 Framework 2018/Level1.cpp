c52deb6e35ebd60b2b731309151dc6e1cecb66ac
Adam Harris <adam_harris@hotmail.com>
2021-03-09 11:24:45 +0000

    Test 1 GUI

--- DX11 Framework 2018/Level1.cpp
+++ DX11 Framework 2018/Level1.cpp
@@ -31,7 +31,13 @@ void Level1::DrawUI()
 {
 	ImGui::Begin("Test 1");
 
-	ImGui::Text("Test 1");
+	Vector3 objPos = _pGameObjects[0]->GetTransform().GetPosition();
+	Vector3 objVel = _pGameObjects[0]->GetBody()->GetVelocity();
+	Vector3 objAcc = _pGameObjects[0]->GetBody()->GetAcceleration();
+
+	ImGui::Text("Object Position: {X: %f, Y: %f, Z: %f}", objPos.x, objPos.y, objPos.z);
+	ImGui::Text("Object Velocity: {X: %f, Y: %f, Z: %f}", objVel.x, objVel.y, objVel.z);
+	ImGui::Text("Object Acceleration: {X: %f, Y: %f, Z: %f}", objAcc.x, objAcc.y, objAcc.z);
 
 	ResetButton();
 
